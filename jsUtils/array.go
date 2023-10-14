68f98a3472fc2dda253da73fe8f798c940bf0a94
devitoDbug <davidOchiengy@gmail.com>
2023-10-14 13:57:58 +0300

    feat: Add utility functions for JavaScript-like operations

--- jsUtils/array.go
+++ jsUtils/array.go
@@ -1,5 +1,29 @@
 package jsUtils
 
-func lastElement(arr []interface{}) interface{} {
+import "errors"
 
+// LastElement returns the last element of the slice.
+func lastElement(arr []interface{}) interface{} {
+	return arr[len(arr)-1]
+}
+
+// Filter returns a new slice with elements that pass the predicate.
+func Filter(arr []interface{}, predicate func(interface{}) bool) []interface{} {
+	var newArr []interface{}
+	for _, value := range arr {
+		if predicate(value) {
+			newArr = append(newArr, value)
+		}
+	}
+	return newArr
+}
+
+// T indexOf returns the index of a given element
+func indexOf[T comparable](arr []T, value T) (int, error) {
+	for index, value := range arr {
+		if value == value {
+			return index, nil
+		}
+	}
+	return -1, errors.New("element not found")
 }
